3ecf686b114067b7c3c70196c28e34de137c3145
Rizky Firman Syah <rizfirsy@gmail.com>
2023-05-20 01:50:34 +0700

    feat: slicing design system(color & typography)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        lamb: "#D87D4A",
+        "dark-gray": "#101010",
+        "light-gray": "#F1F1F1",
+        "super-light-gray": "#FAFAFA",
+        white: "#fff",
+        "light-lamb": "#fbaf85",
+        black: "#000",
       },
     },
   },
   plugins: [],
-}
+};
